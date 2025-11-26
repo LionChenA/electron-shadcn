e941aa5732e09d440435ecd0c527eed34bd3f3a6
LionChenA <2679648321@qq.com>
2025-11-27 00:14:21 +0800

    feat: remove unescessary context and registers

--- src/main/ipc/listenersRegister.ts
+++
@@ -1,8 +0,0 @@
-import type { BrowserWindow } from 'electron';
-import { addThemeEventListeners } from './themeListeners';
-import { addWindowEventListeners } from './windowListeners';
-
-export default function registerListeners(mainWindow: BrowserWindow) {
-  addWindowEventListeners(mainWindow);
-  addThemeEventListeners();
-}
--- src/preload/lib/contextExposer.ts
+++
@@ -1,7 +0,0 @@
-import { exposeThemeContext } from '@/preload/ipc/themeContext';
-import { exposeWindowContext } from '@/preload/ipc/windowContext';
-
-export default function exposeContexts() {
-  exposeWindowContext();
-  exposeThemeContext();
-}
